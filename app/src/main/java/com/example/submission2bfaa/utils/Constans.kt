6efb581f3d27945cb8415c7d5a2c658ef341c078
iqbal <muhiqbal1290@gmail.com>
2021-08-20 20:11:48 +0700

    ignore file

--- app/src/main/java/com/example/submission2bfaa/utils/Constans.kt
+++
@@ -1,7 +0,0 @@
-package com.example.submission2bfaa.utils
-
-class Constans {
-    companion object {
-        const val BASE_URL = "https://api.github.com/"
-    }
-}
--- app/src/main/java/com/example/submission2bfaa/viewmodel/FavoriteViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.submission2bfaa.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.example.submission2bfaa.model.User
-import com.example.submission2bfaa.repository.FavoriteRepositories
-
-//class FavoriteViewModel(private val favoriteRepositories: FavoriteRepositories) : ViewModel() {
-//    val getFavorite = favoriteRepositories.setFavorite()
-//}
